23e058221b38bf816b4bfc6bdd883d27b5d729ab
dec1duous <99750921+dec1duous@users.noreply.github.com>
2022-07-14 22:20:17 +0300

    Create ХЛАМ.cpp

---
+++ ХЛАМ.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>
+#include <iostream>;
+;;;;;;;;;;;;;;;;;;;;;
+int main(int argc, char**& argv)
+{
+  for (int i = 0; i < argc; ++i)
+  {
+    for (int j = 0; j < i; ++j)
+    {
+      std::cout << "#include <" << argv[0] << ">\n";
+    }
+  }
+  return -1102983471;
+}
